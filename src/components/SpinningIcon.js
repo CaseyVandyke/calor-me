02c1ed0d168afed818651437b7112ca8e8c878c3
Cryosith <caseyvandyke@ymail.com>
2020-12-17 12:26:41 -0700

    finished 3js materials

--- src/components/SpinningIcon.js
+++ src/components/SpinningIcon.js
@@ -1,15 +1,36 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 
 import { useFrame } from "react-three-fiber";
+import { softShadows, MeshWobbleMaterial, OrbitControls } from 'drei';
+import { useSpring, a } from 'react-spring/three';
 
-const SpinningIcon = ({position, args, color}) => {
+softShadows();
+
+const SpinningIcon = ({position, args, color, speed}) => {
+    const [expand, setExpand] = useState(false);
+
+    const props = useSpring({
+        scale: expand ? [1.4, 1.4, 1.4] : [1,1,1]
+    })
     const mesh = useRef(null);
     useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
     return (
-        <mesh position={position} ref={mesh}>
-            <boxBufferGeometry attach='geometry' args={args}/>
-            <meshStandardMaterial attach='material' color={color}/>
-        </mesh>
+        <a.mesh 
+        onClick={() => setExpand(!expand)}
+        scale={props.scale}
+        castShadow 
+        position={position} ref={mesh}>
+            <boxBufferGeometry 
+            attach='geometry' 
+            args={args}/>
+            <MeshWobbleMaterial 
+            attach='material' 
+            color={color}
+            speed={speed}
+            factor={1} 
+            />
+            <OrbitControls />
+        </a.mesh>
     )
 }
 
